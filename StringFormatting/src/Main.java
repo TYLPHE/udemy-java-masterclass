ab5b045a284bc84f32ec6386493b886c5568c017
tylphe <tylphet@gmail.com>
2023-12-06 23:25:35 -0700

    Learning about string formatting methods

---
+++ StringFormatting/src/Main.java
@@ -0,0 +1,33 @@
+public class Main {
+    public static void main(String[] args) {
+
+        String bulletIt = "Print a bulleted List:\n" +
+                "\t\u2022 First Point\n" +
+                "\t\t\u2022 Sub Point";
+
+        System.out.println(bulletIt);
+
+        String textBlock = """
+                Print a Bulleted List:
+                    \u2022 First Point
+                        \u2022 Sub point""";
+
+        System.out.println(textBlock);
+
+        int age = 35;
+        System.out.printf("Your age is %d%n", age);
+
+        int yearOfBirth = 2023 - age;
+        System.out.printf("Age = %.2f, %nBirth year = %d%n", (float) age, yearOfBirth);
+
+        for (int i = 1; i <= 100000; i *= 10) {
+            System.out.printf("Printing %6d %n", i);
+        }
+
+        String formattedString = String.format("Your age is %d", age);
+        System.out.println(formattedString);
+
+        formattedString = "Your age is %d".formatted(age);
+        System.out.println(formattedString);
+    }
+}
